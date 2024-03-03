5966863b2abc8adb88f78f565560ad9338204d12
jiongyi <jiongyi.lee@native.org>
2024-03-03 18:19:02 +0800

    Add description

--- test cases/test.spec.ts
+++ test cases/test.spec.ts
@@ -13,6 +13,8 @@
             from_address: '0x88c6C46EBf353A52Bdbab708c23D0c81dAA8134A',
             to_address: '0x697FdEf0996e512A8D1866A657fB407F5f6DA268',
         }
+ *  ETH rpc url: https://eth.llamarpc.com
+ *  Native router contract address: 0xEAd050515E10fDB3540ccD6f8236C46790508A76
  */
 
 
